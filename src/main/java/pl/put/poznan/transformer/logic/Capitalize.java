63552c3732b0f745fe8fc58ea2862f27d0423897
Pawel Perechuda <pawel.perechuda@student.put.poznan.pl>
2021-12-12 22:10:11 +0100

    removed bug from Capitalize class where more than 1 whitespace caused crashes

--- src/main/java/pl/put/poznan/transformer/logic/Capitalize.java
+++ src/main/java/pl/put/poznan/transformer/logic/Capitalize.java
@@ -1,5 +1,8 @@
 package pl.put.poznan.transformer.logic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.put.poznan.transformer.logic.TextTransformer;
+import pl.put.poznan.transformer.rest.TextTransformerController;
 
 
 public class Capitalize extends TextTransformer {
@@ -7,7 +10,9 @@ public class Capitalize extends TextTransformer {
     public String transform(String text){
         String[] str_tab = text.split("\\s");
         for (int i = 0; i < str_tab.length; ++i) {
-            str_tab[i] = Character.toString(str_tab[i].charAt(0)).toUpperCase() + str_tab[i].substring(1);
+            if (str_tab[i].length() > 0) {
+                str_tab[i] = Character.toString(str_tab[i].charAt(0)).toUpperCase() + str_tab[i].substring(1);
+            }
         }
         return String.join(" ", str_tab);
     }
